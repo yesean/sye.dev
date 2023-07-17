8fd922a5d28b1f3afc45d72e27891a4a34169c14
Sean Ye <seanye24@gmail.com>
2023-07-16 20:32:37 -0700

    Initial About page

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+export default () => (
+  <div className="flex h-full items-center justify-center">
+    <ul className="list-disc">
+      <li>front end engineer @ meta</li>
+      <li>ucsd '23</li>
+      <li>i use arch btw</li>
+    </ul>
+  </div>
+);
