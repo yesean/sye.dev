cc5c1ae99823d0b1ed62b539021b174fcb1399a3
Sean Ye <seanye24@gmail.com>
2023-07-20 20:05:36 -0700

    Update work page

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,31 +1,61 @@
+import code from "../assets/code.svg";
+import githubSquare from "../assets/github-square.svg";
+
 const data = [
   {
     name: "QBHub",
-    description: "Quizbowl question reader and clue generator.",
+    description: "quizbowl question reader and clue generator",
     siteURL: "https://qbhub.io",
     githubURL: "https://github.com/yesean/qbhub",
   },
   {
     name: "Pathmazer",
-    description: "Pathfinding and maze generation visualizer.",
+    description: "pathfinding and maze generation visualizer",
     siteURL: "https://pathmazer.com",
     githubURL: "https://github.com/yesean/pathmazer",
+  },
+  {
+    name: "generals.io bot",
+    description: "real-time, multiplayer strategy game bot",
+    siteURL: null,
+    githubURL: "https://github.com/yesean/generalsio-bot",
+  },
+  {
+    name: "dots",
+    description: "configs for my dev stack",
+    siteURL: null,
+    githubURL: "https://github.com/yesean/dotfiles",
   },
 ];
 
 type ItemProps = {
   name: string;
   description: string;
-  siteURL: string;
+  siteURL: string | null;
   githubURL: string;
 };
 
-function Item({ name, description, siteURL }: ItemProps) {
+function Item({ name, description, siteURL, githubURL }: ItemProps) {
   return (
-    <div className="rounded-xl p-6 shadow-lg">
-      <h2 className="text-center text-lg font-bold">
-        <a href={siteURL} target="_blank">
-          {name}
+    <div className="w-full py-7">
+      <h2 className="mr-2 flex place-items-center gap-3 text-lg font-bold">
+        {siteURL != null ? (
+          <a
+            href={siteURL}
+            target="_blank"
+            className="text-sky-500 underline hover:text-sky-600"
+          >
+            {name}
+          </a>
+        ) : (
+          name
+        )}
+        <a
+          target="_blank"
+          href={githubURL}
+          className="rounded p-1 hover:bg-slate-200"
+        >
+          <img src={githubSquare} alt={name} />
         </a>
       </h2>
       {description}
@@ -34,7 +64,7 @@ function Item({ name, description, siteURL }: ItemProps) {
 }
 
 export default () => (
-  <div className="flex h-full flex-col items-center justify-center p-10">
+  <div className="m-auto box-border flex h-full max-w-xs flex-col items-center justify-center divide-y overflow-auto px-6 sm:max-w-lg">
     {data.map((item) => (
       <Item key={item.name} {...item} />
     ))}
